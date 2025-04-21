e333763d468aea1004ec0b9eee75d8b2361918df
Dhananjay gupta <dhananjay.gupta12310@gmail.com>
2025-04-21 10:32:53 +0530

    added base code

---
+++ application.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request, redirect, url_for, flash, Response
+import pymysql
+import re
+import os
+
+app = Flask(__name__)
+app.secret_key = os.urandom(24)
+
+
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+
+application = app  # EB requires 'application' variable
+
+if __name__ == '__main__':
+    application.run(debug=True)
+
